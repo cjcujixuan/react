70d9db2ccdc896382579289dcab0ecdb93d0f424
cjcujixuan <127923959+cjcujixuan@users.noreply.github.com>
2024-02-27 16:04:59 +0800

    Delete src directory

--- src/src/component/GreetingCompent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const name= 'Ryuichi Sakamoto';
-const GreetingComponent = () => {
-    return <h1>Rest in Peace, {name}.</h1>;
-};
-
-export default GreetingComponent;
