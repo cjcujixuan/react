03ad726dec47016913fb69208e05b40764ac63ff
cjcujixuan <127923959+cjcujixuan@users.noreply.github.com>
2024-03-12 15:58:38 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 // import CombinedComponent from './component/CombinedComponent';
 // import CalcOne from './component/Calc';
 // import PropsText from './component/PropsText';
-import DynamicComp from './component/DynamicComp';
+// import DynamicComp from './component/DynamicComp';
+import Students from './component/Students';
 
 
 function App() {
@@ -18,9 +19,10 @@ function App() {
      <CombinedComponent />
      <CalcOne numOne={5} numTwo={7}/>
      <PropsText /> */}
-    <DynamicComp />
+    {/* <DynamicComp />
     <DynamicComp title="研發處" thick="5" color='red' option={true}/>
-    <DynamicComp title="人資處" thick="10" color='blue' option={false}/>
+    <DynamicComp title="人資處" thick="10" color='blue' option={false}/> */}
+    <Students />
     </div>
   );
 }
---
+++ src/component/Students.jsx
@@ -0,0 +1,57 @@
+const Students = () => {
+    const students = [
+        { 'name': 'John', 'score': 90 },
+        { 'name': 'Jane', 'score': 95 },
+        { 'name': 'Peter', 'score': 85 },
+        { 'name': 'Tina', 'score': 75 },
+        { 'name': 'Mike', 'score': 80 },
+        { 'name': 'Sue', 'score': 60 }
+    ];
+    const Change = students.map(student =>
+        student.score < 80 ? 100 : student
+    )
+
+    return (
+        <div>
+            <h1>
+                Student Score Board
+            </h1>
+
+            <h2>
+                Origin Scores
+            </h2>
+            {students.map((student, index) => (
+
+                <div key={index}>
+                    <h4>{student.name}---{student.score}</h4>
+                </div>
+
+            ))}
+
+            <hr />
+
+            {students.map((student, index) => (
+
+                <div key={index}>
+                    <h4>{student.name}---{index % 2 === 0 ? student.score + 10 : student.score - 10}</h4>
+                </div>
+
+            ))}
+
+            <hr />
+
+            {students.map((student, index) => (
+                
+
+                <div key={index}>
+
+                    <h4>{student.name}---{student.score < 80 ? student.score=100 : student.score}</h4>
+                </div>
+
+            ))}
+
+        </div>
+    );
+};
+
+export default Students;
