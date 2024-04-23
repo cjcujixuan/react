1d53543bdd5865301966639f78aa22bc741eb24b
cjcujixuan <127923959+cjcujixuan@users.noreply.github.com>
2024-04-23 16:04:27 +0800

    Add files via upload

--- src/component/CardTest.jsx
+++ src/component/CardTest.jsx
@@ -9,7 +9,8 @@ const CardTest = () => {
         margin: '10px'
     };
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState('');
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -22,21 +23,28 @@ const CardTest = () => {
             });
     }, []);
 
+    const handleFilter = (e) => setFilter(e.target.value);
+    const filteredItems = data.filter(user => user.name.toLowerCase().includes(filter.toLowerCase()));
+
     return (
-        <ul>
-            {data ? (
-                data.map(user => (
-                    <li key={user.id} style={myStyle}>
-                        <h2>{user.name}</h2>
-                        <h3>{user.phone}</h3>
-                        <h5>{user.email}</h5>
-                        <p>{user.website}</p>
-                    </li>
-                ))
-            ) : (
-                <p>資料載入中...</p>
-            )}
-        </ul>
+        <div>
+            <input type='text' value={filter} onChange={handleFilter} placeholder="Filter items..." />
+            <hr />
+            <ul>
+                {data.length > 0 ? (
+                    filteredItems.map(user => (
+                        <li key={user.id} className="card" style={myStyle}>
+                            <h2>{user.name}</h2>
+                            <h3>{user.phone}</h3>
+                            <h5>{user.email}</h5>
+                            <p>{user.website}</p>
+                        </li>
+                    ))
+                ) : (
+                    <p>Data loading...</p>
+                )}
+            </ul>
+        </div>
     );
 }
 
